37e8c345ddbf3d9159f773bf3fcd459b1a3cd3f7
yichen <yichenzhu1337@gmail.com>
2017-07-21 09:03:27 -0400

    junit testing

---
+++ tests/TheTest.java
@@ -0,0 +1,20 @@
+import static org.junit.Assert.*;
+import java.util.Collection;
+import org.junit.After;
+import org.junit.Test;
+
+public class TheTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+    }
+
+    @org.junit.Test
+    public void cannotCreateCompanyWithNullName() throws Exception {
+        assertTrue(false);
+    }
+}
