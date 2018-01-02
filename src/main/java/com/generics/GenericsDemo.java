c1f02f4690a4e3390f8ccc668ff1c1779665ab4d
yichen <yichenzhu1337@gmail.com>
2018-01-01 19:45:36 -0500

    stuff

---
+++ src/main/java/com/generics/GenericsDemo.java
@@ -0,0 +1,19 @@
+package com.generics;
+
+import com.collections.*;
+
+import java.util.*;
+import java.util.Collection;
+
+public class Generics {
+
+    public static void main(String[] args) {
+        List list = new ArrayList();
+        list.add("hello");
+        String s = (String) list.get(0);
+        System.out.println(s);
+
+        Object obj = new Object();
+        obj.
+    }
+}
